26acb6b141055b048a218d41ee1dd965bc3d25fa
SoulJa <superzhaolong@163.com>
2015-11-04 14:46:01 +0800

    first commit

---
+++ SoulJaWeiXin/SoulJaWeiXin/Header.h
@@ -0,0 +1,24 @@
+//
+//  Header.h
+//  SoulJaWeiXin
+//
+//  Created by SoulJa on 15/11/4.
+//  Copyright © 2015年 SoulJa. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+/**
+ *  APPID
+ */
+#define APPID "wxa084afd94e66caa6"
+/**
+ *  APPSECRET
+ */
+#define APPSECRET "b689feed1fb91927eb7c50546728e926"
+/**
+ *  背景颜色
+ */
+#define BACKGROUNDCOLOR UIColor(red: 245/255.0, green: 245/255.0, blue: 245/255.0, alpha: 1.0)
+
+#endif /* Header_h */
